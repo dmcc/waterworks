02ff83cc1eef7bf6d727e58797f285749c8ba30b
David McClosky <dmcc@bigasterisk.com>
2007-05-24 15:43:18 +0000

    HeapQueue: some initial docs
    --HG--
    extra : convert_revision : 811aa0e3a14ad6a610b8ddcc00a501d26add516b

--- HeapQueue.py
+++ HeapQueue.py
@@ -2,8 +2,9 @@ import heapq
 
 class HeapQueue:
     """A more object-oriented way of doing a heapq"""
-    def __init__(self, l=None):
-        self._heap = l or []
+    def __init__(self, initial_list=None):
+        """Create a heap queue, optionally from an initial list."""
+        self._heap = initial_list or []
         heapq.heapify(self._heap)
     def __len__(self):
         return len(self._heap)
@@ -14,8 +15,11 @@ class HeapQueue:
     def __contains__(self, item):
         return item in self._heap
     def peek(self):
+        """Look at the top element."""
         return self._heap[0]
     def pop(self):
+        """Removes and returns the top element."""
         return heapq.heappop(self._heap)
     def add(self, item):
+        """Add an item into the heap."""
         return heapq.heappush(self._heap, item)
