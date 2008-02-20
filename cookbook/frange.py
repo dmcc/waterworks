3d6475eb4fdade17e492148b636a22853f88e934
David McClosky <dmcc@bigasterisk.com>
2008-02-20 19:55:11 +0000

    cookbook.frange: add xfrange (generator version)
    --HG--
    extra : convert_revision : 0a9986fb2dbeb2e54195767b354641bf7a1af254

--- cookbook/frange.py
+++ cookbook/frange.py
@@ -34,3 +34,25 @@ def frange(start, end=None, inc=None):
         L.append(next)
         
     return L
+
+def xfrange(start, end=None, inc=None):
+    """A range function, that does accept float increments and is a
+    generator (i.e. lazy)"""
+
+    if end == None:
+        end = start + 0.0
+        start = 0.0
+
+    if inc == None:
+        inc = 1.0
+
+    count = 0
+    while 1:
+        next = start + count * inc
+        if inc > 0 and next >= end:
+            break
+        elif inc < 0 and next <= end:
+            break
+
+        yield next
+        count += 1
