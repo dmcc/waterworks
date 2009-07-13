ff882a9e2375ddef1030bfc17008c132ac31a20a
David McClosky <dmcc@bigasterisk.com>
2009-07-13 20:55:21 +0000

    README and setup.py updates for version0.2
    --HG--
    extra : convert_revision : 61e0f4ff66bb654d1efd7edc652e70163cc5b3d9

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ if len(sys.argv) < 2:
     sys.argv.append("build")
 
 setup(name = "waterworks",
-      version = "0.1f",
+      version = "0.2",
       maintainer = "David McClosky",
       maintainer_email = "dmcc+py (at) cs.brown.edu",
       description = "David McClosky's utility library",
       packages = ['cookbook', 'waterworks'],
       py_modules = ['AIMA', 'ExitCodes', 'FigUtil', 'Histogram', 'IntRange', 
                     'IntShelve', 'LazyList', 'Selectron', 'Tailer', 'TeXTable', 
-                    'ThreadedJobs', 'TkGeomSavers', 'Utility', 'diffprint', 
-                    'iterextras'],
+                    'ThreadedJobs', 'TkGeomSavers', 'diffprint', 
+                    'iterextras', 'ClusterMetrics', 'FunctionPickler', 
+                    'HeapQueue', 'PrecRec', 'Probably', 'robust_apply', 
+                    'TerminalTitle', 'vimdiff'],
       url='http://cs.brown.edu/~dmcc/software/',
 )
