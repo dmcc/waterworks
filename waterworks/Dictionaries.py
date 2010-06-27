4b04e4a232b81424ad130b9f17cd1331dbfe0b75
David McClosky <dmcc@bigasterisk.com>
2010-06-27 01:19:13 +0000

    waterworks.Dictionaries: improve compatibility, add TwoLevelCounterDict
    --HG--
    extra : convert_revision : 80a2b2c0f702f934e188d7e3385c70fd492b6805

--- waterworks/Dictionaries.py
+++ waterworks/Dictionaries.py
@@ -2,108 +2,113 @@
 # dictionary manipulation #
 ###########################
 
-from collections import defaultdict
+try:
+    from collections import defaultdict
+    _py25_or_better = True
+except ImportError:
+    _py25_or_better = False
 
-class CounterDict(dict):
-    """Simple subclass of a dictionary to help count items:
-    
-    >>> c = CounterDict()
-    >>> c.count('a', 'sequence', 'of', 'things', 'to', 'count')
-    >>> c
-    {'a': 1, 'count': 1, 'sequence': 1, 'of': 1, 'to': 1, 'things': 1}
-    >>> c.count('more', 'things')
-    >>> c
-    {'a': 1, 'count': 1, 'sequence': 1, 'of': 1, 'to': 1, 'things': 2, 'more': 1}
-    >>> c.count('justonething')
-    >>> c
-    {'a': 1, 'count': 1, 'sequence': 1, 'of': 1, 'to': 1, 'justonething': 1, 'things': 2, 'more': 1}
-    >>> c.scale(5)                                                                  >>> c
-    {'a': 5, 'count': 5, 'sequence': 5, 'of': 5, 'to': 5, 'justonething': 5, 'things': 10, 'more': 5}
-    >>> c2 = CounterDict()
-    >>> c2.count_many_times('a', 3)
-    >>> c2
-    {'a': 3}
-    >>> c.merge(c2)
-    >>> c
-    {'a': 8, 'count': 5, 'sequence': 5, 'of': 5, 'to': 5, 'justonething': 5, 'things': 10, 'more': 5}
-    """
-    def count(self, *keys):
-        "Count each item in the sequence 'keys' (which could be a single item)"
-        for key in keys:
-            self.setdefault(key, 0)
-            self[key] += 1
-    def count_many_times(self, key, count):
-        "Count an item 'count' times."
-        self.setdefault(key, 0)
-        self[key] += count
-    def merge(self, *otherdicts):
-        """Merge (add) the counts from other dictionaries into this one."""
-        for otherdict in otherdicts:
-            for key, value in otherdict.items():
+if _py25_or_better:
+    class CounterDict(dict):
+        """Simple subclass of a dictionary to help count items:
+        
+        >>> c = CounterDict()
+        >>> c.count('a', 'sequence', 'of', 'things', 'to', 'count')
+        >>> c
+        {'a': 1, 'count': 1, 'sequence': 1, 'of': 1, 'to': 1, 'things': 1}
+        >>> c.count('more', 'things')
+        >>> c
+        {'a': 1, 'count': 1, 'sequence': 1, 'of': 1, 'to': 1, 'things': 2, 'more': 1}
+        >>> c.count('justonething')
+        >>> c
+        {'a': 1, 'count': 1, 'sequence': 1, 'of': 1, 'to': 1, 'justonething': 1, 'things': 2, 'more': 1}
+        >>> c.scale(5)                                                                  >>> c
+        {'a': 5, 'count': 5, 'sequence': 5, 'of': 5, 'to': 5, 'justonething': 5, 'things': 10, 'more': 5}
+        >>> c2 = CounterDict()
+        >>> c2.count_many_times('a', 3)
+        >>> c2
+        {'a': 3}
+        >>> c.merge(c2)
+        >>> c
+        {'a': 8, 'count': 5, 'sequence': 5, 'of': 5, 'to': 5, 'justonething': 5, 'things': 10, 'more': 5}
+        """
+        def count(self, *keys):
+            "Count each item in the sequence 'keys' (which could be a single item)"
+            for key in keys:
                 self.setdefault(key, 0)
-                self[key] += value
-    def __add__(self, other):
-        """Non-destructive add, returns a new CounterDict"""
-        new_counterdict = self.__class__()
-        new_counterdict.merge(self)
-        new_counterdict.merge(other)
-        return new_counterdict
-    def __iadd__(self, other):
-        """Destructive add, returns None"""
-        self.merge(other)
-    def scale(self, multiplier):
-        """Multiplies all counts by multiplier."""
-        for key in list(self.keys()):
-            self[key] *= multiplier
+                self[key] += 1
+        def count_many_times(self, key, count):
+            "Count an item 'count' times."
+            self.setdefault(key, 0)
+            self[key] += count
+        def merge(self, *otherdicts):
+            """Merge (add) the counts from other dictionaries into this one."""
+            for otherdict in otherdicts:
+                for key, value in otherdict.items():
+                    self.setdefault(key, 0)
+                    self[key] += value
+        def __add__(self, other):
+            """Non-destructive add, returns a new CounterDict"""
+            new_counterdict = self.__class__()
+            new_counterdict.merge(self)
+            new_counterdict.merge(other)
+            return new_counterdict
+        def __iadd__(self, other):
+            """Destructive add, returns None"""
+            self.merge(other)
+        def scale(self, multiplier):
+            """Multiplies all counts by multiplier."""
+            for key in list(self.keys()):
+                self[key] *= multiplier
 
 # these doctests probably don't work due to dictionary random shuffling
-class TwoLevelCounterDict(defaultdict):
-    """Dictionary of CounterDict objects.
+    class TwoLevelCounterDict(defaultdict):
+        """Dictionary of CounterDict objects.
 
-    >>> t = TwoLevelCounterDict()
-    >>> t['outer1'].count('inner1', 'inner2')
-    >>> print t
-    defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer1': {'inner2': 1, 'inner1': 1}})
-    >>> t['outer2'].count('inner1', 'inner3', 'inner4')
-    >>> print t
-    defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer1': {'inner2': 1, 'inner1': 1}, 'outer2': {'inner4': 1, 'inner3': 1, 'inner1': 1}})
-    >>> print t['outer1']
-    {'inner2': 1, 'inner1': 1}
-    >>> t.scale(5)
-    >>> print t
-    defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer1': {'inner2': 5, 'inner1': 5}, 'outer2': {'inner4': 5, 'inner3': 5, 'inner1': 5}})
-    >>> t['outer1'].count('inner1', 'inner2')
-    >>> t['outer2'].count('inner1', 'inner3', 'inner4')
-    >>> t.scale(5)
-    >>> t
-    defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer1': {'inner2': 5, 'inner1': 5}, 'outer2': {'inner4': 5, 'inner3': 5, 'inner1': 5}})
-    >>> t2 = TwoLevelCounterDict()
-    >>> t2['outer2'].count('inner3')
-    >>> t2['outer3'].count('inner10')
-    >>> t2['outer4'].count('inner1', 'inner2')
-    >>> t2['outer4'].count_many_times('inner7', 7)
-    >>> print t2
-    defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer4': {'inner7': 7, 'inner2': 1, 'inner1': 1}, 'outer3': {'inner10': 1}, 'outer2': {'inner3': 1}})
-    >>> t2.merge(t)
-    >>> sorted(t2.keys())
-    ['outer1', 'outer2', 'outer3', 'outer4']
-    >>> print t2
-    defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer4': {'inner7': 7, 'inner2': 1, 'inner1': 1}, 'outer1': {'inner2': 5, 'inner1': 5}, 'outer3': {'inner10': 1}, 'outer2': {'inner4': 5, 'inner3': 6, 'inner1': 5}})
-    """
-    def __init__(self, *args):
-        """Doesn't actually take any arguments (the *args is to get
-        around a pickling problem."""
-        defaultdict.__init__(self, CounterDict)
-    def scale(self, multiplier):
-        """Multiply all inner-dictionaries by multiplier (in place)"""
-        for d in self.values():
-            d.scale(multiplier)
-    def merge(self, *others):
-        all_keys = set(self.keys())
-        for other in others:
-            all_keys.update(other.keys())
-            for key in all_keys:
-                self[key].merge(other[key])
+        >>> t = TwoLevelCounterDict()
+        >>> t['outer1'].count('inner1', 'inner2')
+        >>> print t
+        defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer1': {'inner2': 1, 'inner1': 1}})
+        >>> t['outer2'].count('inner1', 'inner3', 'inner4')
+        >>> print t
+        defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer1': {'inner2': 1, 'inner1': 1}, 'outer2': {'inner4': 1, 'inner3': 1, 'inner1': 1}})
+        >>> print t['outer1']
+        {'inner2': 1, 'inner1': 1}
+        >>> t.scale(5)
+        >>> print t
+        defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer1': {'inner2': 5, 'inner1': 5}, 'outer2': {'inner4': 5, 'inner3': 5, 'inner1': 5}})
+        >>> t['outer1'].count('inner1', 'inner2')
+        >>> t['outer2'].count('inner1', 'inner3', 'inner4')
+        >>> t.scale(5)
+        >>> t
+        defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer1': {'inner2': 5, 'inner1': 5}, 'outer2': {'inner4': 5, 'inner3': 5, 'inner1': 5}})
+        >>> t2 = TwoLevelCounterDict()
+        >>> t2['outer2'].count('inner3')
+        >>> t2['outer3'].count('inner10')
+        >>> t2['outer4'].count('inner1', 'inner2')
+        >>> t2['outer4'].count_many_times('inner7', 7)
+        >>> print t2
+        defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer4': {'inner7': 7, 'inner2': 1, 'inner1': 1}, 'outer3': {'inner10': 1}, 'outer2': {'inner3': 1}})
+        >>> t2.merge(t)
+        >>> sorted(t2.keys())
+        ['outer1', 'outer2', 'outer3', 'outer4']
+        >>> print t2
+        defaultdict(<class 'waterworks.Dictionaries.CounterDict'>, {'outer4': {'inner7': 7, 'inner2': 1, 'inner1': 1}, 'outer1': {'inner2': 5, 'inner1': 5}, 'outer3': {'inner10': 1}, 'outer2': {'inner4': 5, 'inner3': 6, 'inner1': 5}})
+        """
+        def __init__(self, *args):
+            """Doesn't actually take any arguments (the *args is to get
+            around a pickling problem."""
+            defaultdict.__init__(self, CounterDict)
+        def scale(self, multiplier):
+            """Multiply all inner-dictionaries by multiplier (in place)"""
+            for d in self.values():
+                d.scale(multiplier)
+        def merge(self, *others):
+            all_keys = set(self.keys())
+            for other in others:
+                all_keys.update(other.keys())
+                for key in all_keys:
+                    self[key].merge(other[key])
 
 def dictadd(d1, d2):
     """Add the numeric values of values in two dictionaries together in
