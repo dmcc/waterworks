0a7dca5ef21a0340feb891038a0b5cc232eb1169
David McClosky <dmcc@bigasterisk.com>
2011-02-09 23:22:58 +0000

    QuickColorize: cycle through fg and bg colors
    --HG--
    extra : convert_revision : f17a8de6d290d674f115c0507d27c97278ea08aa

--- QuickColorize.py
+++ QuickColorize.py
@@ -5,18 +5,32 @@ text (which will be reused on subsequent calls to colorize()."""
 # TODO use bg colors
 import ansi, itertools
 all_colors = [name for name in dir(ansi) if name[0].isupper()]
-fg_colors = [name for name in all_colors if not name.endswith('BG')]
-bg_colors = [name for name in all_colors if name.endswith('BG')]
+fg_colors = sorted([name for name in all_colors if not name.endswith('BG')])
+bg_colors = sorted([name for name in all_colors if name.endswith('BG')])
 
 for name in 'BLACK BOLD REVERSE RESET'.split():
     fg_colors.remove(name)
 
-fg_color_cycler = itertools.cycle(fg_colors)
+# put black first
+bg_colors.remove('BLACKBG')
+bg_colors.insert(0, 'BLACKBG')
+
+color_pairs = []
+for bg_color in bg_colors:
+    for fg_color in fg_colors:
+        if bg_color.startswith(fg_color):
+            continue
+        if bg_color == 'BLACKBG' and fg_color == 'WHITE':
+            continue
+
+        color_pairs.append((fg_color, bg_color))
+
+color_cycler = itertools.cycle(color_pairs)
 colors_defined = {}
 def colorize(text):
     color = colors_defined.get(text)
     if not color:
-        name = fg_color_cycler.next()
-        color = getattr(ansi, name)
+        names = color_cycler.next()
+        colors = [getattr(ansi, name) for name in names]
         colors_defined[text] = color
-    return color + text + ansi.RESET
+    return ''.join(colors) + text + ansi.RESET
