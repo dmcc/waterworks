b362657d8ff132da8e62097c48d90349d786d1cb
David McClosky <dmcc@bigasterisk.com>
2011-08-02 00:23:23 -0700

    Add get_simple_logger()

--- waterworks/Tools.py
+++ waterworks/Tools.py
@@ -192,3 +192,24 @@ class SimpleKeyEquality:
         return cmp(self._key(), other_key)
     def _key(self):
         raise NotImplementedError("Must implement _key() method.")
+
+def get_simple_logger(name):
+    import logging
+
+    # create logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+    return logger
