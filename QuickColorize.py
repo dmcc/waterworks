f4a613cd08277fba790e380673efff4b412eae44
David McClosky <dmcc@bigasterisk.com>
2014-08-26 17:41:41 -0400

    QuickColorize: various cleanups, randomize colors, add assign_color() Removed __main__ since that's been extracted to quickscripts/quickcolorize

--- QuickColorize.py
+++ QuickColorize.py
@@ -5,77 +5,59 @@ By calling colorize() on text, it will randomly assign a color for
 that text (which will be reused on subsequent calls to colorize().
 Also includes a command-line mode."""
 
-# TODO use bg colors
-import ansi, itertools
-all_colors = [name for name in dir(ansi) if name[0].isupper()]
-fg_colors = sorted([name for name in all_colors if not name.endswith('BG')])
-bg_colors = sorted([name for name in all_colors if name.endswith('BG')])
+import ansi, itertools, random
+def make_cycler():
+    all_colors = [name for name in dir(ansi) if name[0].isupper()]
+    fg_colors = sorted([name for name in all_colors if not name.endswith('BG')])
+    bg_colors = sorted([name for name in all_colors if name.endswith('BG')])
 
-for name in 'BLACK BOLD REVERSE RESET'.split():
-    fg_colors.remove(name)
+    for name in 'BLACK BOLD REVERSE RESET'.split():
+        fg_colors.remove(name)
 
-# put black first
-bg_colors.remove('BLACKBG')
-bg_colors.insert(0, 'BLACKBG')
+    # put black first
+    bg_colors.remove('BLACKBG')
+    bg_colors.insert(0, 'BLACKBG')
 
-color_pairs = []
-for bg_color in bg_colors:
-    for fg_color in fg_colors:
-        if bg_color.startswith(fg_color):
-            continue
-        if bg_color == 'BLACKBG' and fg_color == 'WHITE':
-            continue
+    color_pairs = []
+    nonblackbg_color_pairs = []
+    for bg_color in bg_colors:
+        for fg_color in fg_colors:
+            if bg_color.startswith(fg_color):
+                continue
+            if bg_color == 'BLACKBG' and fg_color == 'WHITE':
+                continue
 
-        color_pairs.append((fg_color, bg_color))
+            if bg_color == 'BLACKBG':
+                color_pairs.append((fg_color, bg_color))
+            else:
+                nonblackbg_color_pairs.append((fg_color, bg_color))
 
-color_cycler = itertools.cycle(color_pairs)
-colors_defined = {}
+    rng = random.Random(0)
+    rng.shuffle(nonblackbg_color_pairs)
+    color_pairs += nonblackbg_color_pairs
+    _color_cycler = itertools.cycle(color_pairs)
+    return _color_cycler
+
+_color_cycler = make_cycler()
+_colors_defined = {}
 def colorize(text):
     """Returns a colorized version of text.  The same text values will
     be consistently colorized."""
-    colors = colors_defined.get(text)
+    text = str(text)
+    colors = _colors_defined.get(text)
     if not colors:
-        names = color_cycler.next()
+        names = _color_cycler.next()
         colors = [getattr(ansi, name) for name in names]
-        colors_defined[text] = colors
+        _colors_defined[text] = colors
     return ''.join(colors) + text + ansi.RESET
 
+def assign_color(text, colors):
+    _colors_defined[text] = colors
+
 if __name__ == "__main__":
-    # a quick command-line program for colorizing stdin or a list of files
-
-    # Example:
-    # % quickcolorize term1 term2 term3 < somefilename
-    # Use -- to separate terms to highlight from filenames:
-    # % quickcolorize term1 term2 term3 -- file1 file2 file3
-    import sys
-
-    filenames = []
-    stdin_mode = True
-
-    mapping = {}
-    for arg in sys.argv[1:]:
-        if arg == '--':
-            stdin_mode = False
-            continue
-        if stdin_mode:
-            mapping[arg] = colorize(arg)
-        else:
-            filenames.append(arg)
-
-    def file_iter():
-        if stdin_mode:
-            yield sys.stdin
-        else:
-            for filename in filenames:
-                f = file(filename)
-                yield f
-                del f
-
-    for input_file in file_iter():
-        for line in input_file:
-            for old, new in mapping.items():
-                line = line.replace(old, new)
-            try:
-                sys.stdout.write(line)
-            except IOError:
-                raise SystemExit
+    import math
+    for digit in str(math.pi):
+        print colorize(digit)
+    print
+    for digit in str(math.e):
+        print colorize(digit)
